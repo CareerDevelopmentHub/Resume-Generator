8689e02e72f2e2f3437ea40eee9c9becdb04c3f4
HarshDDalwadi <102744867+HarshDDalwadi@users.noreply.github.com>
2022-10-07 01:43:21 +0530

    Made functions addNewEdu() and addNewExp()

---
+++ script.js
@@ -0,0 +1,7 @@
+function addNewEdu() {
+    document.getElementById("edu-info").innerHTML += '<br> <textarea rows = "3" id = "edufields" class="form-control edufield" placeholder = "Enter your Education" ></textarea > '
+}
+
+function addNewExp() {
+    document.getElementById("we-info").innerHTML += '<br><textarea rows="3" id="expfield" class="form-control wefield" placeholder="Enter your Work Experience" ></textarea>'
+}
